2ef067a4f1495320ea74b8a029e45206a27e3a10
sadam-04 <asaich3@yahoo.com>
2025-05-13 18:53:47 +0200

    firebase initial setup

--- mad_flutter/lib/main.dart
+++ mad_flutter/lib/main.dart
@@ -3,7 +3,15 @@ import 'package:mad_flutter/create.dart';
 import 'package:mad_flutter/library.dart';
 import 'package:mad_flutter/home.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
